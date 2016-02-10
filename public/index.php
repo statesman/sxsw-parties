59d0ac7698b634264ca5cf4f3257cb24d91df261
Cody Winchester <cjwinchester@gmail.com>
2016-02-10 16:18:49 -0600

    jiggering: fixes #1 ~ fixes #3 ~ fixes #4 ~ fixes #15 ~ fixes #7 ~ fixes #8 ~ fixes #5 ~ fixes #9 ~ fixes #13 ~ fixes #17 ~ fixes #10

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@
   $meta = array(
     "title" => "SXSW side parties | Statesman.com",
     "description" => "Your guide to the best side parties at SXSW 2016.",
-    "thumbnail" => "http://projects.statesman.com/sxsw/assets/share.png", // needs update
     "shortcut_icon" => "http://media.cmgdigital.com/shared/media/2015-11-16-11-32-05/web/site/www_mystatesman_com/images/favicon.ico",
     "apple_touch_icon" => "http://media.cmgdigital.com/shared/theme-assets/242014/www.statesman.com_fa2d2d6e73614535b997734c7e7d2287.png",
     "url" => "http://projects.statesman.com/sxsw/",
@@ -25,22 +24,21 @@
 
   <meta property="og:title" content="<?php print $meta['title']; ?>"/>
   <meta property="og:description" content="<?php print $meta['description']; ?>"/>
-  <meta property="og:image" content="<?php print $meta['thumbnail']; ?>"/>
+  <meta property="og:image" id="fb_img_meta" content="assets/bg_thumb.png"/>
   <meta property="og:url" content="<?php print $meta['url']; ?>"/>
 
   <meta name="twitter:card" content="summary" />
   <meta name="twitter:site" content="@<?php print $meta['twitter']; ?>" />
   <meta name="twitter:title" content="<?php print $meta['title']; ?>" />
   <meta name="twitter:description" content="<?php print $meta['description']; ?>" />
-  <meta name="twitter:image" content="<?php print $meta['thumbnail']; ?>" />
+  <meta name="twitter:image" id="tw_img_meta" content="assets/bg_thumb.png" />
   <meta name="twitter:url" content="<?php print $meta['url']; ?>" />
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="dist/style.css">
 
   <link href='http://fonts.googleapis.com/css?family=Lusitana:400,700,900' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,300,300italic,400italic,700italic,700,800,800italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 
   <?php /* CMG advertising and analytics */ ?>
   <?php include "includes/advertising.inc"; ?>
@@ -48,23 +46,23 @@
 
 </head>
 <body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigaton">
+  <div class="container">
     <div class="navbar-header">
+      <a class="navbar-brand" href="http://www.austin360.com/" target="_blank">
+        <img width="122" height="50" src="assets/logo_austin360_color.png">
+      </a>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-
-        <a class="navbar-brand" href="http://www.statesman.com/" target="_blank">
-        <img class="visible-xs visible-sm" width="103" height="26" src="assets/logo-short-black.png" />
-        <img class="hidden-xs hidden-sm" width="273" height="26" src="assets/logo.png" />
-        </a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
+      <ul class="nav navbar-nav" style="padding-top:5px;">
+        <li><a href="#filter_wrapper">Search</a></li>
+        <li><a href="#" id="loader"></a></li>
         <li class="visible-xs small-social"><a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-facebook-square"></i></a><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo urlencode($meta['url']); ?>&via=<?php print urlencode($meta['twitter']); ?>&text=<?php print urlencode($meta['title']); ?>"><i class="fa fa-twitter"></i></a><a target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-google-plus"></i></a></li>
       </ul>
         <ul class="nav navbar-nav navbar-right social hidden-xs">
@@ -83,7 +81,7 @@
       <h1 id="pagetitle" style="font-weight:bold;">Your unofficial SXSW party guide</h1>
 
       <p class="author">By So Andso</p>
-      <p>Lucas ipsum dolor sit amet boba calrissian amidala sith dooku solo moff organa obi-wan windu. There are <span id="total_count"></span> parties in this guide. Gamorrean binks wedge darth. Mon darth mon kit ponda solo.</p>
+      <p>Lucas ipsum dolor sit amet boba calrissian amidala sith dooku solo moff organa obi-wan windu. There are <span id="total_count"></span> parties in this guide. Gamorrean binks wedge darth. Mon darth mon kit ponda solo. Got a party to add? <a href="mailto:rcorbelli@statesman.com?subject=SXSW%20Party%20Guide">Let us know</a>.</p>
 
       <p class="lead" id="loading"></p>
 
@@ -106,35 +104,21 @@
 <div class="checkbox">
 <label for="free_entry">
     <input type="checkbox" id="free_entry">
-    <span class="label label-success"><i class="fa fa-thumbs-up"></i>&emsp;Free entry</span>
+    <span class="label label-success check_label"><i class="fa fa-thumbs-up"></i>&emsp;Free entry</span>
 </label>
 </div>
 
 <div class="checkbox">
 <label for="free_food">
     <input type="checkbox" id="free_food">
-    <span class="label label-primary"><i class="fa fa-cutlery"></i>&emsp;Free food</span>
+    <span class="label label-primary check_label"><i class="fa fa-cutlery"></i>&emsp;Free food</span>
 </label>
 </div>
 
 <div class="checkbox">
 <label for="rsvp">
     <input type="checkbox" id="rsvp">
-    <span class="label label-info"><i class="fa fa-pencil"></i>&emsp;RSVP required</span>
-</label>
-</div>
-
-<div class="checkbox">
-<label for="staff_pick">
-    <input type="checkbox" id="staff_pick">
-    <span class="label label-warning"><i class="fa fa-star"></i>&emsp;Staff pick</span>
-</label>
-</div>
-
-<div class="checkbox">
-<label for="official">
-    <input type="checkbox" id="official">
-    <span class="label label-danger"><i class="fa fa-shield"></i>&emsp;Badge required</span>
+    <span class="label label-info check_label"><i class="fa fa-pencil"></i>&emsp;RSVP required</span>
 </label>
 </div>
 
@@ -142,66 +126,88 @@
 
 <div class="col-md-6">
 
+<div class="checkbox">
+<label for="staff_pick">
+    <input type="checkbox" id="staff_pick">
+    <span class="label label-warning check_label"><i class="fa fa-star"></i>&emsp;Staff pick</span>
+</label>
+</div>
+
+<div class="checkbox">
+<label for="official">
+    <input type="checkbox" id="official">
+    <span class="label label-danger check_label"><i class="fa fa-shield"></i>&emsp;Badge required</span>
+</label>
+</div>
+
+
+</div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+
+<div class="form-group" style="margin-top:10px;">
+    <label for="day_search">Day</label>
+    <select class="form-control input-lg" id="day_search">
+        <option value="10" selected="selected">Thu (March 10)</option>
+        <option value="11">Fri (March 11)</option>
+        <option value="12">Sat (March 12)</option>
+        <option value="13">Sun (March 13)</option>
+        <option value="14">Mon (March 14)</option>
+        <option value="15">Tue (March 15)</option>
+        <option value="16">Wed (March 16)</option>
+        <option value="17">Thu (March 17)</option>
+        <option value="18">Fri (March 18)</option>
+        <option value="19">Sat (March 19)</option>
+        <option value="20">Sun (March 20)</option>
+    </select>
+</div>
+
+<div id="geo_search_wrapper">
     <div class="form-group" style="margin-top:10px;">
-        <label for="day_search">Day</label>
-        <select class="form-control" id="day_search">
+        <label for="geo_search">Nearby</label>
+        <select class="form-control input-lg" id="geo_search">
             <option value="" selected="selected">All</option>
-            <option value="10">Thu (March 10)</option>
-            <option value="11">Fri (March 11)</option>
-            <option value="12">Sat (March 12)</option>
-            <option value="13">Sun (March 13)</option>
-            <option value="14">Mon (March 14)</option>
-            <option value="15">Tue (March 15)</option>
-            <option value="16">Wed (March 16)</option>
-            <option value="17">Thu (March 17)</option>
-            <option value="18">Fri (March 18)</option>
-            <option value="19">Sat (March 19)</option>
-            <option value="20">Sun (March 20)</option>
+            <option value="1">Within 1 mile of me</option>
+            <option value="0.75">Within 3/4 mile of me</option>
+            <option value="0.5">Within 1/2 mile of me</option>
+            <option value="0.25">Within 1/4 mile of me</option>
         </select>
     </div>
+</div>
 
-    <div id="geo_search_wrapper">
-        <div class="form-group" style="margin-top:10px;">
-            <label for="geo_search">Nearby</label>
-            <select class="form-control" id="geo_search">
-                <option value="" selected="selected">All</option>
-                <option value="1">Within 1 mile of me</option>
-                <option value="0.75">Within 3/4 mile of me</option>
-                <option value="0.5">Within 1/2 mile of me</option>
-                <option value="0.25">Within 1/4 mile of me</option>
-            </select>
-        </div>
     </div>
-
-</div>
 </div>
 
-    <p class="small pull-right"><a id="more_filter_click">
-        <span class="toggle_options">More options &raquo;</span>
-        <span class="toggle_options" style="display:none;">&laquo; Fewer options</span>
-    </a></p>
+    <p class="small pull-right">
+        <a id="more_filter_click">
+            <span class="toggle_options">More options &raquo;</span>
+            <span class="toggle_options" style="display:none;">&laquo; Fewer options</span>
+        </a>
+    </p>
 
     <div id="more_filters" style="display:none;">
 
         <div class="form-group" style="margin-top:10px;">
             <label for="event_search">Event name contains</label>
-            <input type="text" class="form-control" id="event_search">
+            <input type="text" class="form-control input-lg" id="event_search" placeholder="Looking for a specific event?">
         </div>
 
         <div class="form-group" style="margin-top:10px;">
             <label for="venue_search">Venue name contains</label>
-            <input type="text" class="form-control" id="venue_search" placeholder="Looking for a specific venue?">
+            <input type="text" class="form-control input-lg" id="venue_search" placeholder="A venue?">
         </div>
 
         <div class="form-group" style="margin-top:10px;">
             <label for="band_search">Band names contain</label>
-            <input type="text" class="form-control" id="band_search" placeholder="A specific band?">
+            <input type="text" class="form-control input-lg" id="band_search" placeholder="A band?">
         </div>
     </div>
 
       </div>
 
-<button class="btn btn-default btn-block" type="button" id="submit_button">Go &raquo;</button>
+<button class="btn btn-primary btn-lg" type="button" id="submit_button">Go &raquo;</button>
 
 <div id="results_count"></div>
 </div>
@@ -216,13 +222,12 @@
         <% _.each(sxsw, function(d) { %>
             <div class="item" id="<%= d.event_details.id %>">
                 <div class="sharebox">
-                    <p class="bold">
+                    <p class="lead">
                         Share this party
-                        <span class="pull-right">
+                        &emsp;
                             <a href="<%= aas_social(d.event_details.id, d.event_details.name, d.event_details.time, d.event_details.date, d.venue_details.name).tw %>" target="_blank"><i class="fa fa-twitter"></i></a>
                             &ensp;
                             <a href="<%= aas_social(d.event_details.id, d.event_details.name, d.event_details.time, d.event_details.date, d.venue_details.name).fb %>" target="_blank"><i class="fa fa-facebook-square"></i></a>
-                        </span>
 
                     </p>
                 </div>
