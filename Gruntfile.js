2b62c0106c8e3516d5eb56b61364d6e8b45d89e6
Cody Winchester <cjwinchester@gmail.com>
2016-02-15 10:47:41 -0600

    a new approach -- splash, grid, big buttons

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,8 @@ module.exports = function(grunt) {
             'bower_components/bootstrap/js/button.js',
             'bower_components/bootstrap/js/collapse.js',
             'bower_components/bootstrap/js/dropdown.js',
-            'bower_components/bootstrap/js/transition.js',            'bower_components/underscore/underscore-min.js',
+            'bower_components/bootstrap/js/transition.js',
+            'bower_components/underscore/underscore-min.js',
             'bower_components/masonry/dist/masonry.pkgd.min.js',
             'src/js/main.js'
           ]
